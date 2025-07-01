78b72ecb416f642c10399d2dd1d11bc19820f722
Manuel C. V. <97321235+Mancasvel@users.noreply.github.com>
2025-07-01 21:59:58 +0200

    fix(factoria): minor fixes factoria checkers

--- src/exercises/FactoriaVisitas.java
+++ src/exercises/FactoriaVisitas.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import utiles.Checkers;
+
 public class FactoriaVisitas {
 	
 	public static Visita parseaVisita(String lineaCsv) {
@@ -11,6 +13,7 @@ public class FactoriaVisitas {
 		String[] partes = lineaCsv.split(";");
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
 		
+		Checkers.check("Debe haber 7 partes", partes.length == 7);
 		String email = partes[0].trim();
 		String ciudad = partes[1].trim();
 		String codigoPostal = partes[2].trim();
