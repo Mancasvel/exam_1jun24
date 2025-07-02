db9f86c1772519755b547192a536c5ab82fe79cd
Manuel C. V. <97321235+Mancasvel@users.noreply.github.com>
2025-07-02 13:37:39 +0200

    feat: minor fix

--- src/exercises/Evaluacion.java
+++ src/exercises/Evaluacion.java
@@ -5,7 +5,6 @@ import utiles.Checkers;
 public record Evaluacion(String hamburgueseria, Integer presentacion, Integer puntoCarne, Integer calidadIngredientes, Integer calidadPan) {
 	
 	public Evaluacion{
-		Double evaluacionFinal;
 		
 		Checkers.check("El valor de la valoracion de la presentacion debe estar comprendido entre 0 y 10", (presentacion>=0 && presentacion<=10));
 		Checkers.check("El valor de la valoracion del punto de la carne debe estar comprendido entre 0 y 10", (puntoCarne>=0 && puntoCarne<=10));
